3fc943f553980664038b5a2a89e6c522febd3ec1
alaattinatac <alaattinatac@hotmail.com>
2022-02-02 08:25:12 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import BlogComp from './components/Blog'
 
 function App() {
+  let headerone = <div>LET HEADER ONE</div>
+  let content = <div>
+    <h1>App.js</h1>
+    <p>Prf <b>Bold</b> <i>ITALIC</i> {5 / 2} <b>--</b> {5 / 2 == 2.5 ? "YES" : "NO"}</p>
+    <img src={logo} alt="Logo" className="App-logo" />
+    {headerone}
+  </div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {content}
+      <BlogComp />
     </div>
   );
 }
---
+++ src/components/Blog.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import BlogItem from "./BlogItem";
+
+class Blog extends Component {
+  render() {
+    return <div>
+        Blog JS
+        <BlogItem/>
+        <BlogItem/>
+        <BlogItem/>
+    </div>
+  }
+}
+
+export default Blog
